e1669a3db89ea9fd7825794e60dced34498881e8
danielmrey <info@danielmrey.com>
2018-02-14 14:08:58 +0000

    A options for banner
    Banner can be now set up to be fixed (large) or bigger (full).

--- functions/hooks/build-hooks-banner.php
+++ functions/hooks/build-hooks-banner.php
@@ -29,20 +29,33 @@ if ( !function_exists('progenitor_build_banner') ) {
 /*----------------------------------------------------------------------------*\
   SITE BANNER IMAGE
   -----------------
-  Loads Feature Image
-  If Full image is bigger than Large Size then it loads Large Size
-  Otherwise it load the Full image
-  (This can be modified to allow images to fit the browser window)
-
+  Loads Feature Image:
+	> progenitor_build_banner_image('full') [default] : Loads the Full image
+	  (then there is a CSS protection added for very large screens)
+	> progenitor_build_banner_image('large') : If Full image width is bigger than Large image width then it loads Large image, Else loads the Full image
+    (this is due to web performance)
 \*----------------------------------------------------------------------------*/
 
 if ( !function_exists('progenitor_build_banner_image') ) {
-  function progenitor_build_banner_image() {
-    $img_full = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full');
-    if ($img_full[1] > 1110) {
-      the_post_thumbnail('large');
-    } else {
-      the_post_thumbnail();
-    }
+  function progenitor_build_banner_image($size='full') {
+		$size = strtolower ($size);	
+		$img_full = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full');
+		$large_width = get_option('large_size_w');		
+		
+		if ($size == 'full') {
+		  $img_full_1 = $img_full[1] + 1;
+      echo '<style>@media only screen and (min-width: '.$img_full_1.'px) {.site-banner-in {width:'.$large_width.'px; margin:0 auto;}} </style>';		
+			echo '<div class="site-banner-in">';					
+			the_post_thumbnail();
+			echo '</div>';			
+		}
+		else if ($size == 'large') {
+			if ($img_full[1] > $large_width) {
+				the_post_thumbnail('large');
+			} else {
+				the_post_thumbnail();
+			}
+		}
+		
   }
 };
