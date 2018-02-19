a15b03a2bb01fc5405b40c7e3c5e7c7aa0a38ff6
danielmrey <info@danielmrey.com>
2018-02-19 17:32:52 +0000

    Fancybox

---
+++ functions/fancybox.php
@@ -0,0 +1,11 @@
+<?php
+/**!
+ * Fancybox
+ */
+
+if ( !function_exists('progenitor_fancybox') ) {
+  function progenitor_fancybox() {
+
+    require get_template_directory() . '/functions/fancybox/fancybox.php';
+  }
+};
