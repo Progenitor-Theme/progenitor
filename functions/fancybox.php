9513d0819888fc0f688015037da1c07d46a86197
danielmrey <info@danielmrey.com>
2018-02-19 18:15:18 +0000

    Fancybox: add caption to pop up image
    I am not sure if this is the best place to add this.

--- functions/fancybox.php
+++ functions/fancybox.php
@@ -5,6 +5,16 @@
 
 if ( !function_exists('progenitor_fancybox') ) {
   function progenitor_fancybox() {
+    
+		// Add title to anchor from image title (only works with images which are placed in a native WP Gallery)
+		// https://wordpress.org/support/topic/add-the-title-of-an-image-to-the-image-link-in-gallery	
+		function add_title_attachment_link($link, $id = null) {
+			$id = intval( $id );
+			$_post = get_post( $id );
+			$post_title = esc_attr( $_post->post_title );
+			return str_replace('<a href', '<a title="'. $post_title .'" href', $link);	
+		}
+		add_filter('wp_get_attachment_link', 'add_title_attachment_link', 10, 2);	    
 
     require get_template_directory() . '/functions/fancybox/fancybox.php';
   }
