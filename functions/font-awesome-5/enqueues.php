ddecf9de42c1f099332f68c820a86fbbf93230a7
Simon Padbury <simon.padbury@icloud.com>
2018-02-19 18:45:55 +0000

    Update enqueues.php

--- functions/font-awesome-5/enqueues.php
+++ functions/font-awesome-5/enqueues.php
@@ -5,7 +5,7 @@
 
 function progenitor_icons_enqueues() {
 
-  wp_register_script('font-awesome-5-config-js', get_template_directory_uri() . '/functions/icons/font-awesome-5/config.js', false, null, null);
+  wp_register_script('font-awesome-5-config-js', get_template_directory_uri() . '/functions/font-awesome-5/config.js', false, null, null);
   wp_enqueue_script('font-awesome-5-config-js');
 
   wp_register_script('font-awesome', 'https://use.fontawesome.com/releases/v5.0.6/js/all.js', false, '5.0.6', null);
