0f67a6d61c503822f54b9e2e600870b8761fc5b1
danielmrey <info@danielmrey.com>
2018-02-20 13:16:43 +0000

    Woocommerce

---
+++ functions/woocommerce/woocommerce.php
@@ -0,0 +1,7 @@
+<?php
+/**!
+ * Woocommerce
+ */
+
+ require get_template_directory() . '/functions/woocommerce/enqueues.php';
+ require get_template_directory() . '/functions/woocommerce/woocommerce-setup.php';
